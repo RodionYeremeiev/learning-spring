c5ee2b731bc4da98916b33636dc8c634733e385b
Rod <ryitlearning@gmail.com>
2024-07-10 23:29:42 +0300

    [chapter 2] - add Room and Room repository

---
+++ src/main/java/com/lil/spring/learning_spring/data/RoomRepository.java
@@ -0,0 +1,7 @@
+package com.lil.spring.learning_spring.data;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoomRepository extends CrudRepository<Room, Long> {}
---
+++ src/main/java/com/lil/spring/learning_spring/util/AppStartupEvent.java
@@ -0,0 +1,24 @@
+package com.lil.spring.learning_spring.util;
+
+import com.lil.spring.learning_spring.data.Room;
+import com.lil.spring.learning_spring.data.RoomRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+public class AppStartupEvent implements ApplicationListener<ApplicationReadyEvent> {
+    private final RoomRepository roomRepository;
+
+    public AppStartupEvent(RoomRepository roomRepository) {
+        this.roomRepository = roomRepository;
+    }
+
+    @Override
+    public void onApplicationEvent(ApplicationReadyEvent event) {
+        Iterable<Room> rooms = this.roomRepository.findAll();
+        rooms.forEach(room -> log.info(String.valueOf(room)));
+    }
+}
